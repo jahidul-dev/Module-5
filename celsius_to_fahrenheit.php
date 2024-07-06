bb14ff0ff5022dd088202e2f03c5c3ad7978bec6
jahidul-dev <gmj.islam@gmail.com>
2024-07-07 00:10:58 +0600

    Add files via upload

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,10 @@
+<?php
+//Celsius Value
+$celsius = 32;
+
+// Convert
+$fahrenheit = ($celsius * 9/5) + 32;
+
+printf("The Fahrenheit temperature is: %.2f", $fahrenheit);
+
+?>
---
+++ largest_number.php
@@ -0,0 +1,17 @@
+<?php
+	//Set Value
+	$num1 = 4;
+	$num2 = 5;
+	$num3 = 6;
+
+	//largest number 
+	if($num1 >= $num2 && $num1 >= $num3){
+		$largest = $num1;
+	}elseif ($num2 >= $num1 && $num2 >= $num3) {
+		$largest = $num2;
+	}else {
+		$largest = $num3;
+	}
+
+	echo "largest number is: $largest";
+?>
